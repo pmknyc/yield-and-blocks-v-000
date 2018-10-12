f0ac0a25d7f79d47ab14cc627e8f519a95ec25ff
Paula McKinley <pmknyc@optimum.net>
2018-10-12 01:33:06 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,12 +11,9 @@ def hello_t(array)
   end
 end
 
-
-=begin
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
-=end
